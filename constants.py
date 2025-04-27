82316f17b28083767b2b647c8de8f8caa83fd609
Andreea Roxana Patrascu <roxandreeapatrascu17@gmail.com>
2025-04-27 21:11:49 +0300

    Add a new character in the list and create a .py file with it

---
+++ constants.py
@@ -0,0 +1,13 @@
+# Character list
+CHARACTER_LIST = [
+    "Albus Dumbledore",
+    "Severus Snape",
+    "Harry Potter",
+    "Lord Voldemort",
+    "Hermione Granger",
+    "Hagrid",
+    "Dobby",
+    "Ron Weasley",
+    "Luna Lovegood",
+    "Sirius Black"
+]
