1ea4f789cd4720a38a724380d081f16c3199e25e
Andreea Roxana Patrascu <roxandreeapatrascu17@gmail.com>
2025-04-27 21:22:00 +0300

    Update constants.py with keywords for precheck

--- constants.py
+++ constants.py
@@ -11,3 +11,11 @@ CHARACTER_LIST = [
     "Luna Lovegood",
     "Sirius Black"
 ]
+
+# Keywords for Harry Potter related topics
+HARRY_POTTER_KEYWORDS = [
+    "harry", "hogwarts", "dumbledore", "ron", "hermione", "voldemort", "snape", "sirius", "luna", "hagrid",
+    "granger", "weasley", "dobby", "wizard", "magic", "quidditch", "wand", "spell", "potion",
+    "house", "gryffindor", "slytherin", "ravenclaw", "hufflepuff", "diagon alley", "muggle",
+    "goblin", "elf", "broomstick"
+]
