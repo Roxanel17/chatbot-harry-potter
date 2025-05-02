53295a000d0202a155bf4297674ea6cc95181f61
Andreea Roxana Patrascu <roxandreeapatrascu17@gmail.com>
2025-05-02 11:07:46 +0300

    Update init file

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,10 @@
 # src/__init__.py
 # This is a placeholder file to mark this directory as a Python package.
-from .helpers import get_character_prompt
-from .constants import CHARACTER_LIST, HARRY_POTTER_KEYWORDS
+from .helpers import get_character_prompt, get_character_greeting
+from .constants import ( 
+    CHARACTER_LIST, 
+    HARRY_POTTER_KEYWORDS, 
+    CHARACTER_AVATARS, 
+    CHARACTER_HOUSE, 
+    HOUSE_COLORS
+)
